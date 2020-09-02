9ca3bfef0919a8965ed9476d8f5fa0b4dce422e5
christinelai97 <70616608+christinelai97@users.noreply.github.com>
2020-09-02 16:00:01 -0400

    Program comments
    Added comments to explain java code

--- Lab1.java
+++ Lab1.java
@@ -1,6 +1,13 @@
+//This is a sample program that prints out Hello world
+
 public class Lab1.java {
+  
+  //program executes by calling method main()
   public static void main(String [] args) {
+    
     System.out.println("Hello, world!");
+    //println adds a new line after string
+    //text displayes as "Hello, world!\n"
   }
 }
  
